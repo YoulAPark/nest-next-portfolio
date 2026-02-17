d4dd618aa771e3436596c785933f024b081c78d5
youlapark <urdepone@gmail.com>
2026-02-17 17:45:16 +0900

    feat: Next 서버에서 API 연동 추가

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,24 @@
-export default async function HomePage() {
-    const test_json = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/test`, {
+import Link from 'next/link'
+import styles from './page.module.scss'
+
+export default async function Main() {
+    // `${process.env.NEXT_PUBLIC_API_URL}/test`
+    const response = await fetch(`http://localhost:3001/`, {
         cache: 'no-store',
     })
-
-    const test = await test_json.json()
-
+    const data = await response.json()
+    
     return (
-        <main>
-            <h1>메인 페이지</h1>
-            <p>서버상태 : {test.status}</p>
-        </main>
+        <div className={styles.main}>
+            <section data-role="left">
+                <p>메인 페이지</p>
+                <p>{JSON.stringify(data)}</p>
+            </section>
+
+            <section data-role="right">
+                <p>오른쪽!</p>
+                <p>{JSON.stringify(data)}</p>
+            </section>
+        </div>
     )
 }
