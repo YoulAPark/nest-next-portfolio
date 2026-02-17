957a7aef22eb9cd6a99bf746b4c16dada3ff9e6d
youlapark <urdepone@gmail.com>
2026-02-17 17:44:40 +0900

    chore: dev 실행 구조 안정화 및 포트 설정 변경
    ###  package.json 수정
    - pnpm dev 실행 구조 변경
    
    ### 포트 변경
    - 백엔드 포트충돌 가능성으로 인한 포트번호 변경 (5000 -> 3001)
    
    ### 바인딩 추가
    - Docker 대비 0.0.0.0 바인딩 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 5000)
+
+    /**
+     * Cross-Origin 요청 허용 설정
+     * Next.js(3000) → Nest(3001)
+     */
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+
+    /**
+     * 0.0.0.0으로 바인딩하여 추후 Docker 작동시 접근 가능하도록 설정
+     */
+    const host = '0.0.0.0'
+    const port = Number(process.env.PORT) || 3001
+
+    await app.listen(port, host)
 }
 bootstrap()
